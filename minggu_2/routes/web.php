b21727fe3f676de6d135104e442cf227789f13a5
alfin <occpangastuti202@gmail.com>
2025-02-24 22:10:27 +0700

    pos

--- minggu_2/routes/web.php
+++ minggu_2/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Route; // mengimpor kelas Route untuk mendefinisikan rute
+use App\Http\Controllers\ItemController; // mengimpor ItemController untuk menangani permintaan terkait item
+use App\Http\Controllers\UserProfileController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('items', ItemController::class); // resource controller dengan nama items dari controller ItemController
+
+Route::get('/user/profile', function () {
+    return 'selamat datang';
+   })->name('profile');
+
+   Route::get('/user/profile',[UserProfileController::class, 'index']
+   )->name('profile');
